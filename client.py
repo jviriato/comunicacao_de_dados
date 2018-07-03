7f3e2c6739d2109ef1106aa347a92f104005b83f
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-07-03 13:19:23 -0300

    ranii.jpg

--- client.py
+++ client.py
@@ -4,14 +4,20 @@ import socket
 import sys
 
 class Client:
-    TCP_IP = '127.0.0.1'
-    TCP_PORT = 4242
-    BUFFER_SIZE = 1024
-    
-    def start_connection:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
-        
+    def __init__(self):
+        self.TCP_IP = '127.0.0.1'
+        self.TCP_PORT = 4242
+        self.startConnection()
 
-    def stop_connection:
-        s.close()
+    def start_connection(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def close_connection(self):
+        self._socket.close()
+
+    def send_msg(self):
+        msg = raw_input()
+        while msg != '\x18':
+            self._socket.send(msg)
+            msg = raw_input()
+        self.close_connection()
