f178b8274f76ffc35cba80d770e7c3d19bb0bc2b
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-07-03 14:25:23 -0300

    connection refused

--- client.py
+++ client.py
@@ -4,20 +4,35 @@ import socket
 import sys
 
 class Client:
-    def __init__(self):
+    def __init__(self, filename):
         self.TCP_IP = '127.0.0.1'
-        self.TCP_PORT = 4242
+        self.TCP_PORT = 5010
+        self.origem = (self.TCP_IP, self.TCP_PORT)
+        self.file = open(filename, 'rb')
+
         self.start_connection()
 
     def start_connection(self):
+        print("Conectado")
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.connect(self.origem)
 
     def close_connection(self):
+        self.file.close()
         self._socket.close()
 
     def send_msg(self):
-        msg = raw_input()
-        while msg != '\x18':
-            self._socket.send(msg)
-            msg = raw_input()
+        bytes_file = self.file.read(1024)
+        while(bytes_file):
+            self._socket.send("bytes_file")
+            bytes_file = self.file.read(1024)
         self.close_connection()
+
+
+def main():
+    c = Client('raniery.jpg')
+    c.send_msg()
+
+if __name__ == '__main__':
+    main()
+    
--- server.py
+++ server.py
@@ -8,11 +8,34 @@ class Server:
 
     def __init__(self):
         self.TCP_IP = '127.0.0.1'
-        self.TCP_PORT = 4242
+        self.TCP_PORT = 5010
+        self.destino = (self.TCP_IP, self.TCP_PORT)
+
         self.start_connection()
 
     def start_connection(self):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.bind(self.destino)
 
     def close_connection(self):
         self._socket.close()
+
+    def receive_msg(self):
+        self._socket.listen(5)
+        f = open("rani.jpg", 'wb')
+        while True:
+            c, addr = self._socket.accept()
+            print("Conexao de:" + addr)
+            l = c.recv(1024)
+            while(l):
+                f.write(l)
+                l = c.recv(1024)
+            f.close()
+
+def main():
+    s = Server()
+    s.receive_msg()
+
+if __name__ == '__main__':
+    main()
+    
