9ec2b9785a65cf7591b015653edffaac550a6dd8
lanabr <lanarossato@hotmail.com>
2018-07-08 14:47:14 -0300

    delimitação do frame com erro no checksum

--- client.py
+++ client.py
@@ -22,12 +22,29 @@ class Client:
         self._socket.close()
 
     def send_msg(self):
-        bytes_file = self.file.read(1024)     #bytes_file = 1KB do arquivo
-        while(bytes_file):                    #enquanto restar arquivo
-            self._socket.send(bytes_file)     #manda os KB
-            bytes_file = self.file.read(1024) #reseta os bytes
+        bytes_file = self.file.read(1024)         # bytes_file = 1KB do arquivo
+        while (bytes_file):                       # enquanto restar arquivo
+            frame = self.delimitacao_frame(bytes_file) # monta o frame
+            self._socket.send(frame)              # manda o frame
+            bytes_file = self.file.read(1024)     # reseta os bytes
         self.close_connection()
 
+    def checksum(self):
+        checksum = 1
+
+
+    def delimitacao_frame(self, bytes_file):      # @ header data trailer @
+        ini_end = '@'                             # inicio e fim do frame
+        frame = ini_end                           # frame começa com @
+        frame = frame + str(bytes_file)           # coloca os dados no frame
+        frame = frame + str(self.checksum())      # coloca o checksum no frame
+        frame = frame + ini_end                   # frame termina com @
+
+        return frame
+
+        # header = endereços de origem e destino e outras infos de controle   header = go back n ARQ
+        # trailer = bits redundantes para detecção e correção de erros        trailer = ARQ, checksum
+        # data = + byte-stuffing
 
 def main():
     c = Client('raniery.jpg')
